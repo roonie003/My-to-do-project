5c042d9c312f05767e0e2e1512a21a070ca3c010
Roonie <roonierue@gmail.com>
2021-07-27 16:48:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -10,9 +10,13 @@ const taskInput = document.querySelector('#task');
 
 loadEventListeners();
 
-//add task event
+
 
 function loadEventListeners() {
+//DOM Load event
+  document.addEventListener('DOMContentLoaded' , getTasks);
+
+//add task event
     form.addEventListener('submit', addTask);
 //remove task event
  taskList.addEventListener('click', removeTask);
@@ -21,6 +25,49 @@ clearBtn.addEventListener('click', clearTasks);
 
 //filter task event
 filter.addEventListener('keyup', filterTasks);
+
+}
+
+// get tasks from Local Storage
+
+function getTasks(e){
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    } else {
+        task = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.forEach(function(task){
+         // create li element
+
+    const li = document.createElement('li');
+    //add class
+
+     li.className ='collection-item';
+
+    //create text node and append to li
+     li.appendChild(document.createTextNode(task));
+
+    //create new link element
+
+     const link = document.createElement('a');
+
+    //add class
+
+    link.className = 'delelet-item secondary-content';
+
+    // add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    
+    //append the link to li
+
+    li.appendChild(link);
+
+    //append li to ul
+    taskList.appendChild(li);
+
+    });
 
 }
 
@@ -58,11 +105,31 @@ function addTask(e) {
     //append li to ul
     taskList.appendChild(li);
 
+
+
+    // store in local storage
+
+    storeTaskInLocalStorage(taskInput.value);
+
     //clear input
 
     taskInput.value = '';
 
     e.preventDefault();
+}
+
+//Store Task
+function storeTaskInLocalStorage(task){
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    } else {
+        task = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    task.push(task);
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 //remove task
@@ -83,4 +150,23 @@ while(taskList.firstChild){
      }
 
     }
+
+
+//filter tasks
+
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach();
+    (function(task) {
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) != -1){
+               task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    })
+}
+
+
 }
